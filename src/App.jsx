6892173a8b41fa375f7bd15298a160c216d7ed41
brandon <brandon.mulligan47@gmail.com>
2025-04-16 13:40:10 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Hello, World!</h1>
+        <button type="button">Click me!</button>
+
+      </header>
+    </>
+  );
+}
+
+export default App;
