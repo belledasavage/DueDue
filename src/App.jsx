1c3d0e49378c38c958f817620b8f8e172c327b5a
brandon <brandon.mulligan47@gmail.com>
2025-04-17 15:04:00 -0400

    the button changes the name

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import "./App.css";
+import { useState } from 'react';
+
 
 
 
 function App(props) {
-  
+  const [name, setName] = useState("Bob");
+
+  function changeName(e) {
+    if (name == "Brandon") {
+      setName("Bob")
+    }
+    else {
+      setName("Brandon")
+
+    }
+  }
+
+
   return (
     <>
       <header>
-        <h1>Hello, {props.subject}</h1>
-        <button type="button">Click me!</button>
+        <h1>Hello, {name}</h1>
+        <button type="button" className="primary" onClick={changeName}>
+          Click me!
+
+        </button>
 
       </header>
     </>
