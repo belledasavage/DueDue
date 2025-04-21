f4b29a9ee0d08b7e11e10b07c023f0eb3e11971c
wirlydev <wirleydev@gmail.com>
2025-04-21 13:37:01 -0400

    add react-bootstrap and rearrange some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { useState } from 'react';
+import { useState } from "react";
 import ListAdder from "./components/listAdder";
-
 
 function App(props) {
   const [name, setName] = useState("Bob");
 
   function changeName(e) {
     if (name == "Brandon") {
-      setName("Bob")
-    }
-    else {
-      setName("Brandon")
-
+      setName("Bob");
+    } else {
+      setName("Brandon");
     }
   }
 
-
   return (
     <>
-      <header>
-        <h1>Hello, {name}</h1>
-        <button type="button" className="primary" onClick={changeName}>
-          Click me!
-
-        </button>
-          <ListAdder/>
-      </header>
+      <div className="container container-xxl">
+        <div class="row">
+          <div className="col">
+            <h1>Hello, {name}</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div className="col">
+            <button type="button" className="primary" onClick={changeName}>
+              Click me!
+            </button>
+            <ListAdder />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
 
 export default App;
-
